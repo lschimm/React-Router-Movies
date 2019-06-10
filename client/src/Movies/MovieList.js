92e3c290142e4091570bb611d0a78b697a2c3f2f
lschimm <lschimmelfennig@gmail.com>
2019-06-10 15:31:09 -0500

    finishing up part 2

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -34,8 +35,10 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <div className="movie-card">]
+    <Link to={`/movies:`}>
       <h2>{title}</h2>
+      </Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
